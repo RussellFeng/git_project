3d32d275a8cdf9ac8dd47542181e0998f6f81851
fz <406698538@qq.com>
2021-02-13 10:54:53 +0800

    insert

---
+++ exercise/insert_sort.py
@@ -0,0 +1,18 @@
+def insert_sort(nums):
+    '''
+    插入排序
+    '''
+    n = len(nums)
+    for i in range(1, n):
+        j = i
+        while j > 0 and nums[j] < nums[j-1]:
+            nums[j], nums[j-1] = nums[j-1], nums[j]
+            j -= 1
+       
+    
+    
+    
+if __name__ == '__main__':
+    a = [3,2,1,8,2,5,2]
+    insert_sort(a)
+    print(a)
